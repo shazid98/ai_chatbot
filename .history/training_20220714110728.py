9ec00ceaf44a1dbc1e5336acf6a007200c3520ee
shazid98 <shazid.tipu@gmail.com>
2022-07-14 14:28:34 +0100

    update

---
+++ .history/training_20220714110728.py
@@ -0,0 +1,20 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letter = ['?', '!', '.', ',']
